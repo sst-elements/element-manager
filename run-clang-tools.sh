285e1be66d4e2ecfb4aefc7da0b7f5a48716730d
sabbirahm3d <sabbir0ahmed0@gmail.com>
2020-04-27 16:12:30 -0400

    add options for clang-tool driver

--- run-clang-tools.sh
+++ run-clang-tools.sh
@@ -1,20 +1,29 @@
 #! /bin/zsh
 
-cp -r ${REPO_DIR}/sst-elements/src/sst/elements/${1}/* ${REPO_DIR}/${1} && cd ${REPO_DIR}/${1}
-find . \( -type d -name ".deps" -o -name ".dirstamp" \) -exec rm -rf {} \;
-
-find . -type f -exec sed -i "s/sst\/elements\/${1}\///g" {} \;
-find . -type f -exec sed -i "s/<sst_config.h>/<sst\/core\/sst_config.h>/g" {} \;
-
-source fix-headers.sh
-cd -
-
-if [ "${@[-1]}" = "--script" ]; then
-    run-clang-tidy.py -p ${1}/build -fix
-    run-clang-tidy.py -p ${1}/build -checks="-*,modernize-use-override" -fix
-else
-    find ${1}/ \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) -exec clang-tidy -p ${1}/build -header-filter="${1}/*" -fix-errors -fix {} \;
-    find ${1}/ \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) -exec clang-tidy -p ${1}/build -header-filter="${1}/*" -checks="-*,modernize-use-override" -fix-errors -fix {} \;
+if [ "${@[-1]}" = "--copy" ]; then
+    cp -r ${REPO_DIR}/sst-elements/src/sst/elements/${1}/* ${REPO_DIR}/${1} && cd ${REPO_DIR}/${1}
+    find . \( -type d -name ".deps" -o -name ".dirstamp" \) -exec rm -rf {} \;
 fi
 
-find ${1} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) -exec clang-format -i {} \;
+if [ "${@[-1]}" = "--headers" ]; then
+    cd ${REPO_DIR}/${1}
+    find . -type f -exec sed -i "s/sst\/elements\/${1}\///g" {} \;
+    find . -type f -exec sed -i "s/<sst_config.h>/<sst\/core\/sst_config.h>/g" {} \;
+    source fix-headers.sh
+fi
+
+if [ "${@[-1]}" = "--tidy" ]; then
+    run-clang-tidy.py -p ${REPO_DIR}/${1}/build -fix
+    run-clang-tidy.py -p ${REPO_DIR}/${1}/build -checks="-*,modernize-use-override" -fix
+elif [ "${@[-1]}" = "--tidy2" ]; then
+    find ${REPO_DIR}/${1}/ \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
+        -exec clang-tidy -p ${REPO_DIR}/${1}/build -header-filter="${1}/*" -fix-errors -fix {} \;
+    find ${REPO_DIR}/${1}/ \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
+        -exec clang-tidy -p ${REPO_DIR}/${1}/build -header-filter="${1}/*" \
+        -checks="-*,modernize-use-override" -fix-errors -fix {} \;
+fi
+
+if [ "${@[-1]}" = "--format" ]; then
+    find ${REPO_DIR}/${1} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
+        -exec clang-format -i {} \;
+fi
