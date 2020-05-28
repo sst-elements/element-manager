12441ecaf0e9b9e9db8daff8ad3d8a02b013299d
sabbirahm3d <sab61r.a@gmail.com>
2020-05-28 09:54:33 -0400

    minor update to internal vars

--- run-clang-tools.sh
+++ run-clang-tools.sh
@@ -1,36 +1,38 @@
 #! /bin/zsh
 
+ELEM_DIR=${ORG_DIR}/${1}
+
 if [ "${@[-1]}" = "--copy" ]; then
-    cp -r ${REPO_DIR}/sst-elements/src/sst/elements/${1}/* ${REPO_DIR}/${1} && cd ${REPO_DIR}/${1}
+    cp -r ${REPO_DIR}/${1}/* ${ELEM_DIR} && cd ${ELEM_DIR}
     find . \( -type d -name ".deps" -o -name ".dirstamp" \) -exec rm -rf {} \;
 fi
 
 if [ "${@[-1]}" = "--headers" ]; then
-    find ${REPO_DIR}/${1} -type f -exec sed -i "s/sst\/elements\/${1}\///g" {} \;
-    find ${REPO_DIR}/${1} -type f -exec sed -i "s/<sst_config.h>/<sst\/core\/sst_config.h>/g" {} \;
+    find ${ELEM_DIR} -type f -exec sed -i "s/sst\/elements\/${1}\///g" {} \;
+    find ${ELEM_DIR} -type f -exec sed -i "s/<sst_config.h>/<sst\/core\/sst_config.h>/g" {} \;
     if [[ -a ${1}/fix-headers.sh ]]; then
         source ${1}/fix-headers.sh
     fi
 fi
 
 if [ "${@[-1]}" = "--tidy" ]; then
-    cp .clang-tidy ${REPO_DIR}/${1}
-    run-clang-tidy.py -p ${REPO_DIR}/${1}/build -header-filter="${REPO_DIR}/${1}/*" -fix
-    run-clang-tidy.py -p ${REPO_DIR}/${1}/build -header-filter="${REPO_DIR}/${1}/*" -checks="-*,modernize-use-override" -fix
-    rm ${REPO_DIR}/${1}/.clang-tidy
+    cp .clang-tidy ${ELEM_DIR}
+    run-clang-tidy.py -p ${ELEM_DIR}/build -header-filter="${ELEM_DIR}/*" -fix
+    run-clang-tidy.py -p ${ELEM_DIR}/build -header-filter="${ELEM_DIR}/*" -checks="-*,modernize-use-override" -fix
+    rm ${ELEM_DIR}/.clang-tidy
 elif [ "${@[-1]}" = "--tidy2" ]; then
-    cp .clang-tidy ${REPO_DIR}/${1}
-    find ${REPO_DIR}/${1} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
-        -exec clang-tidy -p ${REPO_DIR}/${1}/build -header-filter="${REPO_DIR}/${1}/*" -fix-errors {} \;
-    find ${REPO_DIR}/${1} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
-        -exec clang-tidy -p ${REPO_DIR}/${1}/build -header-filter="${REPO_DIR}/${1}/*" \
+    cp .clang-tidy ${ELEM_DIR}
+    find ${ELEM_DIR} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
+        -exec clang-tidy -p ${ELEM_DIR}/build -header-filter="${ELEM_DIR}/*" -fix-errors {} \;
+    find ${ELEM_DIR} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
+        -exec clang-tidy -p ${ELEM_DIR}/build -header-filter="${ELEM_DIR}/*" \
         -checks="-*,modernize-use-override" -fix-errors {} \;
-    rm ${REPO_DIR}/${1}/.clang-tidy
+    rm ${ELEM_DIR}/.clang-tidy
 fi
 
 if [ "${@[-1]}" = "--format" ]; then
-    cp .clang-format ${REPO_DIR}/${1}
-    find ${REPO_DIR}/${1} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
+    cp .clang-format ${ELEM_DIR}
+    find ${ELEM_DIR} \( -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) \
         -exec clang-format -i {} \;
-    rm ${REPO_DIR}/${1}/.clang-format
+    rm ${ELEM_DIR}/.clang-format
 fi
